908f4dd841fcbe68d0a63a9136e082404d87e9be
Oleksii Kucheruk <oleksii.kucheruk+github@teamdev.com>
2019-05-28 09:55:49 +0300

    Add gradle project environment.

---
+++ build.gradle.kts
@@ -0,0 +1,96 @@
+/*
+ * Gradle build automation script written using Kotlin DSL
+ *
+ * Written by: Oleksii Kucheruk <oleksii.kucheruk@teamdev.com> on Fri May 17 2019
+ */
+
+/*
+ * Create read-only ext variables
+ */
+val checkstyleVersion by extra { "8.20" }
+val checkstyleConfigFile by extra { "config/checkstyle/google_checks.xml" }
+
+val junitApiVersion by extra { "5.4.2" }
+
+val mainClass by extra { "AnalogWatch" }
+val jacocoVersion by extra { "0.8.3" }
+val minimumBundleCoverage by extra { 0.5 }
+val minimumClassCoverage by extra { 0.5 }
+
+val guavaVersion by extra { "27.1-jre" }
+
+val slf4jApiVersion by extra { "1.7.26" }
+
+plugins {
+    idea
+    java
+    checkstyle // Static code analysis for Java source code
+    jacoco // Java Code Coverage Library
+    `build-dashboard`
+    id("org.sonarqube") version "2.7" // Continuous inspection of code quality
+}
+
+group = "AnalogWatch"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testCompile("org.junit.jupiter:junit-jupiter-api:" + junitApiVersion)
+    testImplementation("org.junit.jupiter:junit-jupiter-api:" + junitApiVersion)
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:" + junitApiVersion)
+    compile("org.slf4j:slf4j-api:" + slf4jApiVersion)
+    compile("org.slf4j:slf4j-log4j12:"+ slf4jApiVersion)
+    implementation("com.google.guava:guava:" + guavaVersion)
+}
+
+checkstyle {
+    toolVersion = checkstyleVersion
+    configFile = file(checkstyleConfigFile)
+    isIgnoreFailures = true
+}
+
+jacoco {
+    toolVersion = jacocoVersion
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "okucheruk_AnalogWatch")
+        property("sonar.organization","okucheruk-github")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.login", "fde76f534e76294c8728bd40ab37f58030800f12")
+        property("sonar.branch.name", "master")
+        // property("sonar.branch.target", "master") // commented because the main(master) branch must not have a target
+        property("sonar.java.checkstyle.reportPaths",
+                "build/reports/checkstyle/main.xml,build/reports/checkstyle/test.xml")
+    }
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks {
+    /*
+    * Enable required JaCoCo reports formats (HTML, XML, CSV).
+    * Note: build-dashboard plugin depends on these setting;
+    * coveralls plugin depends on xml format report
+    */
+    val jacocoTestReport by getting(JacocoReport::class) {
+        reports.html.isEnabled = true
+        reports.xml.isEnabled = true
+        reports.csv.isEnabled = false
+    }
+
+    /*
+     * Enable native support for JUnit.
+     * Add to test task jacocoTestReport execution
+     */
+    val test by getting(Test::class) {
+        useJUnitPlatform()
+        finalizedBy(jacocoTestReport)
+    }
+}
